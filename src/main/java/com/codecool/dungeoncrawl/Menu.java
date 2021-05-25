4a453c02e3176fb022f6d108e85d4cc63914a296
Yilmaz Ádám Timur <ptcadam@gmail.com>
2021-05-25 17:40:15 +0200

    clean up

--- src/main/java/com/codecool/dungeoncrawl/Menu.java
+++ src/main/java/com/codecool/dungeoncrawl/Menu.java
@@ -157,24 +157,4 @@ public class Menu {
             text.setFill(b ? Color.WHITE : Color.GREY);
         }
     }
-
-    private static class TriCircle extends Parent {
-        public TriCircle() {
-            Shape shape1 = Shape.subtract(new Circle(5), new Circle(2));
-            shape1.setFill(Color.WHITE);
-
-            Shape shape2 = Shape.subtract(new Circle(5), new Circle(2));
-            shape2.setFill(Color.WHITE);
-            shape2.setTranslateX(5);
-
-            Shape shape3 = Shape.subtract(new Circle(5), new Circle(2));
-            shape3.setFill(Color.WHITE);
-            shape3.setTranslateX(2.5);
-            shape3.setTranslateY(-5);
-
-            getChildren().addAll(shape1, shape2, shape3);
-
-            setEffect(new GaussianBlur(2));
-        }
-    }
 }
