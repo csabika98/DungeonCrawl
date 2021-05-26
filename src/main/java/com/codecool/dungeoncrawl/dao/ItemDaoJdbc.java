5c49e95e4fe14ec99fa91bcd5ebbf3dd5804633c
sallcsacsaba8 <sallcsacsaba8@penguin>
2021-05-26 14:22:52 +0200

    itemdao

--- src/main/java/com/codecool/dungeoncrawl/dao/ItemDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/ItemDaoJdbc.java
@@ -1,4 +1,28 @@
 package com.codecool.dungeoncrawl.dao;
 
-public class ItemDaoJdbc {
+import com.codecool.dungeoncrawl.model.ItemModel;
+
+import java.util.List;
+
+public class ItemDaoJdbc implements ItemDao{
+// WITH THIS DO THE REST!
+    @Override
+    public void add(ItemModel item) {
+
+    }
+
+    @Override
+    public void update(ItemModel player) {
+
+    }
+
+    @Override
+    public ItemModel get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<ItemModel> getAll() {
+        return null;
+    }
 }
